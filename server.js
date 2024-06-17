ae4232957e252b4bb56c8a630b2076f0ca4f4915
E-troll <williams.rferreira@gmail.com>
2024-06-17 00:48:33 -0300

    Corrigir (ERRADO)

--- server.js
+++ server.js
@@ -4,18 +4,33 @@ const app = express()
 app.use(express.json())
 
 const Livros = []
+const users = []
+
+app.post('/users', (req, res) =>{
+
+    users.push(req.body)
+
+    res.send('Login Efetuado com sucesso')
+})
+
+app.get('/users', (req, res) =>{
+    res.status(200).json(users)
+})
 
 app.post('/Livros', (req, res) =>{
 
     Livros.push(req.body)
 
-    res.send('OK Tudo Certo !')
+    res.send('Livro inserido com Sucesso !')
 })
 
 app.get('/Livros', (req, res) =>{
-    res.json(Livros)
+    res.status(200).json(Livros)
 })
 
 app.listen(3000)
 
 
+/*  
+u5EmJtMNNlMSNWgo
+*\
