0e991ea3e356989d851c316bf1aaffd40de9325d
E-troll <williams.rferreira@gmail.com>
2024-06-17 15:45:27 -0300

    Edição do Prisma

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.post('/users', async (req, res) => {
-  const user = await prisma.user.create({
+ await prisma.user.create({
     data: {
       email: req.body.email,
       Nome: req.body.Nome,
@@ -14,17 +14,17 @@ app.post('/users', async (req, res) => {
     },
   });
 
-  res.status(201).json(user);
+  res.status(201).json(req.body);
   
 });
 
 app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.status(200).json(users);
+  const user = await prisma.user.findMany();
+  res.status(200).json(user);
 });
 
 app.post('/Livros', async (req, res) => {
-  const livro = await prisma.post.create({
+  await prisma.post.create({
     data: {
       ISBN: req.body.ISBN,
       titulo: req.body.titulo,
@@ -33,12 +33,12 @@ app.post('/Livros', async (req, res) => {
     },
   });
 
-  res.status(201).json(livro);
+  res.status(201).json(req.body);
 });
 
 app.get('/Livros', async (req, res) => {
-  const livros = await prisma.post.findMany();
-  res.status(200).json(livros);
+  const livro = await prisma.post.findMany();
+  res.status(200).json(livro);
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
