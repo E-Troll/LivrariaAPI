85328913205a00a075b831e91aa4a1e21c2b2456
E-troll <williams.rferreira@gmail.com>
2024-06-18 19:45:04 -0300

    18/06

--- server.js
+++ server.js
@@ -22,6 +22,20 @@ app.get('/users', async (req, res) => {
   res.status(200).json(user);
 });
 
+app.put('/users/:email', async (req, res) => {
+  const updatedUser = await prisma.user.update({
+    where: {
+      email: req.params.email
+    },
+    data: {
+      email: req.body.email,
+      Nome: req.body.Nome
+    }
+  });
+
+  res.status(200).json(updatedUser);
+});
+
 app.post('/Livros', async (req, res) => {
   await prisma.post.create({
     data: {
@@ -38,6 +52,22 @@ app.post('/Livros', async (req, res) => {
 app.get('/Livros', async (req, res) => {
   const livro = await prisma.post.findMany();
   res.status(200).json(livro);
+});
+
+app.put('/Livros/:ISBN', async (req, res) => {
+  const updatedLivro = await prisma.livro.update({
+    where: {
+      ISBN: req.params.ISBN
+    },
+    data: {
+      ISBN: req.body.ISBN,
+      titulo: req.body.titulo,
+      Categoria: req.body.Categoria,
+      Autor: req.body.Autor
+    }
+  });
+
+  res.status(200).json(updatedLivro);
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
